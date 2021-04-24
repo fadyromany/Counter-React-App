73e2c51a452776b54bff8155fadb79a32c021cb3
Fady Romany <fadyromany@hotmail.com>
2021-04-24 10:28:42 +0200

    change state values

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
 
   state = {
     counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
+      { id: 1, value: 5 },
+      { id: 2, value: 5 },
+      { id: 3, value: 5 },
+      { id: 4, value: 5 },
     ]
   };
 
